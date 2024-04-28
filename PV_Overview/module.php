a7aa676d0a27350934531ab9f4bcc177eab44564
da8ter <58776097+da8ter@users.noreply.github.com>
2024-04-28 12:32:05 +0200

    a

--- PV_Overview/module.php
+++ PV_Overview/module.php
@@ -222,7 +222,7 @@ class TileVisuPhotovoltaikOverviewTile extends IPSModule
 
     public function RequestAction($Ident, $Value) {
         //SetValue('Zeitraum', $Value);
-        $this->SetValue("Zeitraum", $Value);
+        SetValue("Zeitraum", $Value);
         $this->GetFullUpdateMessage();
     }
     
